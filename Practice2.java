465a7d649136307ffcdaecc48f76120ec954e748
Jyoti Kumari <jk1076387@gmail.com>
2025-07-16 14:55:53 +0530

    Create Practice2.java

---
+++ Practice2.java
@@ -0,0 +1,46 @@
+// for loop practice questions
+import java.util.*;
+ public class Practice2{
+     public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        /* System.out.println("Enter a number:");
+         int n=sc.nextInt();
+         System.out.println("Even numbers upto "+n+" are");
+         for(int i=0; i<n;i++){
+             if(i%2==0)
+             System.out.println(i);
+         }*/
+         /*System.out.println("Enter a number:");
+         int a=sc.nextInt();
+         System.out.println("Odd numbers upto "+a+" are:");
+         for(int i=0;i<a;i++){
+             if(i%2!=0)
+                 System.out.println(+i+" ");
+         }*/
+         /*System.out.println("Enter a number:");
+         int n=sc.nextInt();
+         int sum=0;
+         for(int i=0;i<n;i++){
+             sum+=i;
+             System.out.println("sum:" +sum);
+         }//if printing statement of sum is inside loop then we will able to see each process step by step:)
+          System.out.println("sum:" +sum);
+          //if printing statement of sum is outside loop then we will able to see only result not step:)
+         */
+        /* System.out.println("Enter a number:");
+         int a=sc.nextInt();
+         int diff=0;
+         for(int i=0;i<a;i++){
+             diff-=i;
+         }
+         System.out.println(diff);  
+         }*/
+         System.out.print("Enter a number:");
+         int n=sc.nextInt();
+         int a=0;
+         for(int i=1;i<10;i++){
+             a=n*i;
+              System.out.println(a);
+         }
+     }
+ }
