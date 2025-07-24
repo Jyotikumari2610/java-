70e87e77bf04797439c9385430118d92a2263e9b
Jyoti Kumari <jk1076387@gmail.com>
2025-07-24 14:22:13 +0530

    Create #9.java

---
+++ #9.java
@@ -0,0 +1,92 @@
+import java.util.*;
+public class Functions{
+    /* public static float printAverage(int a,int b,int c){
+        float average=(a+b+c)/3;
+        System.out.println(average);
+        return average;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three number:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        printAverage(a,b,c);
+    } */
+   /* public static int sumOfOdd(int n){
+        int sum=0;
+    for(int i=0;i<n;i++){
+        if(i%2!=0){
+        sum+=i;
+    }
+    }
+    return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        System.out.println(sumOfOdd(n));
+
+    } */
+/*     public static int greaterOne(int a,int b){
+        
+        if(a>b){
+        System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+        return 1;
+         
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter two number:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        greaterOne(a,b);
+
+    } */
+    
+}
+import java.util.*;
+  public class Function{
+    /* public static int helloCircle(int radius){
+        float pie= 3.142f;
+        float circumfrence=0;
+        circumfrence=2*pie*radius;
+        System.out.println(circumfrence);
+        return 1;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius of circle:");
+        int radius= sc.nextInt();
+        helloCircle(radius);
+    } */
+   /* public static int voteEligibility(int age){
+        if(age>18){
+            System.out.println("Eligible to vote");
+        }
+        else {
+            System.out.println("Not Eligible");
+        }
+        return 0;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age of person:");
+        int age=sc.nextInt();
+        voteEligibility(age);
+    } */
+     public class Condition{
+    public static void main(String args[]){
+        int i=0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+            while(true);
+    }
+}
