0c71a4347b6d0f1d59ed0daafdfa40590fa8e94e
Jyoti Kumari <jk1076387@gmail.com>
2025-07-28 07:36:25 +0530

    Update #11.java

--- #11.java
+++ #11.java
@@ -42,3 +42,36 @@ for(int i=0;i<size;i++){
 }
 }
 }
+import java.util.*;
+public class Arrays{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+       /* int arr[]=new int[5];
+        System.out.println("Enter the  elemets of array of 5 integers:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println("The integers in the array is:"+arr[i]);
+    }*/
+   /* System.out.println("Enter size of an array:");
+    int size=sc.nextInt();
+    int num[]=new int[size];
+    System.out.println("Ente elements in array:");
+    for(int i=0;i<num.length;i++){
+        num[i]=sc.nextInt();
+    }
+    for(int i=0;i<size;i++){
+        System.out.println("Entered array is:"+num[i]);
+    }*/
+    int arr[]=new int[6];
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<arr.length;i++){
+        sum += arr[i];
+    }
+    System.out.println("Sum of all elements in array is:"+sum);
+}
+}
