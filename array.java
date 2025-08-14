d2b007ed321fd0e907ac3efac7e8221b4b72852e
Jyoti Kumari <jk1076387@gmail.com>
2025-08-14 21:01:51 +0530

    Update array.java

--- array.java
+++ array.java
@@ -102,4 +102,65 @@ for(int i=0;i<arr.length;i++){
     }
     }
 }
-
+import java.util.*;
+public class LargestSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+    /*    int largest = arr[0];
+        int smallest = arr[0];
+        for (int i = 1; i < size; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        System.out.println("Largest element: " + largest);
+        System.out.println("Smallest element: " + smallest);
+        */
+     /*   int evenCount = 0, oddCount = 0;//to count how many no are even or odd
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    */
+  /*  System.out.print("Enter element to search: ");
+        int a =sc.nextInt();
+         boolean found=false;
+        for (int i = 0; i < size; i++) {
+            if (arr[i]==a) {
+                System.out.println("Element found at index " +i);
+                found =true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found in array.");
+    } */
+     for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp =arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1]= temp;
+                }
+            }  }
+        System.out.println("Array in ascending order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+}
+}
