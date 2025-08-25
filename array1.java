9184a1bb273000583a66ee1319f8ecaabc4390b0
Jyoti Kumari <jk1076387@gmail.com>
2025-08-25 23:41:32 +0530

    Update array1.java

--- array1.java
+++ array1.java
@@ -168,3 +168,34 @@ public class Palindrome{
         }
     }
 }
+import java.util.Scanner;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for(int i=0; i< n; i++){
+            arr[i]=sc.nextInt();
+        }
+       int max=arr[0];
+        int min=arr[0];
+        int sum=0;
+        for(int i = 0; i < n; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            sum+=arr[i];
+        }
+        double average=(double)sum/n;
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+        System.out.println("Average of elements: " + average);
+        
+    }
+}
