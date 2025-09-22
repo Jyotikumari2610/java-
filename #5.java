711e889f1b97e78079636250784acc9209a8217a
Jyoti Kumari <jk1076387@gmail.com>
2025-09-22 22:32:15 +0530

    Update #5.java

--- #5.java
+++ #5.java
@@ -58,3 +58,31 @@ public class Welcome2{
                 else
                         System.out.println(n+"Not prime Number!!");
         }                                                                                                                                                                 }   
+import java.util.*;
+public class Practice{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elments of array:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int even=arr[0];
+        int odd=arr[0];
+       for(int i=0;i<n;i++){
+           if(i%2==0){
+               even++;
+           }
+           else if(i%2!=0){
+               odd++;
+           }
+          else {
+              System.out.println("Invalid input");
+          }
+       System.out.println("Even elements are:"+even);
+       System.out.println("Odd elements are:"+odd);
+       }
+    }
+}
