8968e125e399de021ea01c789389be110c7c5944
Jyoti Kumari <jk1076387@gmail.com>
2025-10-02 20:58:13 +0530

    Update array1.java

--- array1.java
+++ array1.java
@@ -256,3 +256,36 @@ public class UniqueElementsArray {
         }
     }
 }
+import java.util.*;
+public class Sorting{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        int num[]=new int[n];
+        System.out.println("Enter elements of array:");
+        for(int i=0;i<num.length;i++){
+            num[i]=sc.nextInt();
+        }
+     /*   //Duplicate array
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++){
+                if(num[i]==num[j]){
+                    System.out.println("Duplicate array are:"+num[i]);
+                }
+            }
+        }
+        */
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++){
+                if(num[i]>num[j]){
+                    int temp=num[i];
+                    num[i]=num[j];
+                    num[j]=temp;
+                }
+            }
+        }
+        Arrays.sort(num);
+        System.out.println("Sorted Array: "+Arrays.toString(num));
+    }
+}
