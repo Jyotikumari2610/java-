b1ad1e13e56397339cec7c5d739fed7995b9f014
Jyoti Kumari <jk1076387@gmail.com>
2025-10-04 22:33:40 +0530

    Update 2d_array.java

--- 2d_array.java
+++ 2d_array.java
@@ -177,3 +177,78 @@ public class Main{
      }
 }
 }
+import java.util.Scanner;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows and columns: ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int a[][] = new int[r][c];
+        int b[][] = new int[r][c];
+        int result[][] = new int[r][c];
+
+        System.out.println("Enter first matrix:");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                a[i][j] = sc.nextInt();
+
+        System.out.println("Enter second matrix:");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                b[i][j] = sc.nextInt();
+
+        // Addition
+        System.out.println("\nAddition:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] = a[i][j] + b[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // Subtraction
+        System.out.println("\nSubtraction:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] = a[i][j] - b[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+}
+            System.out.println();
+        }
+        // Multiplication
+        System.out.println("\nMultiplication: ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] = a[i][j] * b[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Division 
+        System.out.println("\nDivision: ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (b[i][j] != 0)
+                    System.out.print((a[i][j] / b[i][j]) + " ");
+                else
+                    System.out.print("∞ ");
+            }
+            System.out.println();
+        }
+
+        // Transpose of first matrix
+        System.out.println("\nTranspose of first matrix: ");
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < r; j++) {
+                System.out.print(a[j][i] + " ");
+            }
+            System.out.println();
+        }
+}
+}
