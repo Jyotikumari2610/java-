b3eed235cfa8baf56c78b3d9766042b6f4e7307b
Jyoti Kumari <jk1076387@gmail.com>
2025-10-09 21:43:33 +0530

    Update 12.java

--- 12.java
+++ 12.java
@@ -27,3 +27,53 @@ public class Sorting {
         }
     }
 }
+import java.util.*;
+public class BubbleSortInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    /*    for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        */
+      /*  for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] < arr[j+1]) { 
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array (Descending): " + Arrays.toString(arr));
+        */
+        
+        boolean swapped;
+        for(int i=0; i<n-1; i++){
+            swapped = false;
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped) break; 
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
