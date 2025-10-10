8c0d02a73da30378d89295f4398ec00400eac165
Jyoti Kumari <jk1076387@gmail.com>
2025-10-10 22:18:29 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -83,3 +83,19 @@ public class StudentReport {
         System.out.println("\nTopper: " + students[0].name + " with " + students[0].marks + " marks!");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum: " + maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
