2ab2b6c8c75dcfc933f5dcd28de6620e813ab582
Jyoti Kumari <jk1076387@gmail.com>
2025-10-13 21:47:55 +0530

    Update 12.java

--- 12.java
+++ 12.java
@@ -77,3 +77,22 @@ public class BubbleSortInput {
         System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 }
+import java.util.Arrays;
+import java.util.Scanner;
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first word: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second word: ");
+        String str2 = sc.nextLine();
+        char[] a = str1.toLowerCase().toCharArray();
+        char[] b = str2.toLowerCase().toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if (Arrays.equals(a, b))
+            System.out.println("Anagram!");
+        else
+            System.out.println("Not an Anagram!");
+    }
+}
