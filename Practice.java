5a92442c59967d2f025d40d71345a2ec85ca0f6a
Jyoti Kumari <jk1076387@gmail.com>
2025-10-22 21:41:47 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -39,4 +39,56 @@ public class Practice{
         }
         }
     }
+import java.util.*;
+public class PrimeNumber {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       /* int num=sc.nextInt();
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+             flag = true;
+             break;
+        }
+    }
+      if (!flag)
+        System.out.println(num + " is a prime number.");
+      else
+        System.out.println(num + " is not a primenumber.");
+        */
+        
+      /*  int base =sc.nextInt();
+        int exponent=sc.nextInt();
+        long result = 1;
+          while (exponent != 0) {
+            result *= base;--exponent;
+            }
+        System.out.println("Answer = " + result);
+        */
+     /*   char ch =sc.next().charAt(0);
+        int ascii=ch;
+         System.out.println("The ASCII value of " + ch + " is: "+ ascii);
+ */
+/*    int n1=sc.nextInt();
+    int n2=sc.nextInt();
+    int lcm = (n1 > n2) ? n1 : n2;
+        while (true) {
+          if (lcm % n1 == 0 && lcm % n2 == 0) {
+            System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+                 break;
+                 }
+                ++lcm;
+           }
+           */
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+            }
+         }
+    public static int fibonacci(int n) {
+        if (n <= 1)
+         return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+ }
 
