14540a307f34359f848934548219cdbf452a06b8
Jyoti Kumari <jk1076387@gmail.com>
2025-11-06 21:08:11 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -36,3 +36,34 @@ public  class Solution {
         System.out.println("Program Ended!");
     }
 }
+import java.util.*;
+
+public class SelectionSortDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " numbers:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n - 1; i++) {
+            int minIndex = i;  
+            int j = i + 1;  
+            while(j < n) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+                j++;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println("Sorted Array:");
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
