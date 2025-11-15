395ffaee836b5765c01846e49d9509014afc4ea4
Jyoti Kumari <jk1076387@gmail.com>
2025-11-15 23:47:58 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -99,3 +99,21 @@ public class Main {
         return maxSoFar;
     }
 }
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        return ;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+      arrayTraversal(arr);
+    }
+}
