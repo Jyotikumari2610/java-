e57ad45f81e8a01fd6d5812eed374f404884b813
Jyoti Kumari <jk1076387@gmail.com>
2025-12-06 22:20:20 +0530

    Update functions.java

--- functions.java
+++ functions.java
@@ -259,4 +259,34 @@ class Solution {
         obj.countDigits(n);
     }
 }
-
+// User function Template for Java
+import java.util.*;
+class Solution {
+    public static boolean areEqual(int a[], int b[]) {
+        // Your code here
+        if(a.length != b.length){
+            return false;
+        }
+        for(int i=0;i<=a.length;i++){
+                if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int b[]=new int[n];
+        for(int i=0;i<=n;i++){
+                a[i]=sc.nextInt();
+            }
+        for(int i=0;i<=n;i++){
+                b[i]=sc.nextInt();
+            }
+        
+       Solution obj=new Solution();
+       obj.areEqual(a,b);
+    }
+}
