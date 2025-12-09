e87d538b098338d7099d63df814256af316bab8c
Jyoti Kumari <jk1076387@gmail.com>
2025-12-09 21:46:57 +0530

    Update functions.java

--- functions.java
+++ functions.java
@@ -290,3 +290,22 @@ class Solution {
        obj.areEqual(a,b);
     }
 }
+import java.util.*;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest=-1;
+        int scndLargest=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }}
+            for(int i=0;i<arr.length;i++){
+            if(arr[i]>scndLargest && arr[i]!=largest){
+                scndLargest=arr[i];
+            }
+        }
+        return scndLargest;
+    }
+    
+}
