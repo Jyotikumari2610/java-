e400d10582e021b4aad7ad101cff8809f5962656
Jyoti Kumari <jk1076387@gmail.com>
2025-12-14 21:09:09 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -13,3 +13,30 @@ public class FactorialExample {
         System.out.println("Factorial of " + number + " is: " + result);
     }
 }
+import java.util.*;
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int n=arr.length;
+        int temp[]=new int[n];
+        for(int i=0;i<=n;i++){
+            temp[i]=arr[n-i-1];
+        }
+        for(int i=0;i<=n;i++){
+            arr[i]=temp[i];
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<=n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        Solution obj=new Solution();
+        obj.reverseArray(arr);
+    }
+}
