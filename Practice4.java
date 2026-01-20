6f37ba6c4b926164474b53f6c6b6f79a7733ffeb
Jyoti Kumari <jk1076387@gmail.com>
2026-01-20 22:03:48 +0530

    Update Practice4.java

--- Practice4.java
+++ Practice4.java
@@ -42,4 +42,51 @@ import java.util.*;
               System.out.println(a);
          }
      }
-                                  }
+ }
+
+import java.util.*;
+  public class Function{
+public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    int positiveCount=0;
+    int negativeCount=0;
+    int zeroCount=0;
+    char choice;
+    do{
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+       if(n>0){
+           positiveCount++;
+    }
+    else if(n<0){
+        negativeCount++;
+    }
+    else {
+        zeroCount++;
+    }
+    n++;
+    System.out.println("Do u wnt to continue?y/n:");
+     choice=sc.next().charAt(0);
+    }
+    while(choice=='y' || choice=='Y');
+    
+   System.out.println(positiveCount);
+   System.out.println(negativeCount);
+   System.out.println(zeroCount);
+}
+}
+import java.util.*;
+public class Wlcm{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two number:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(+a+"*" +i+"="+(a*i));
+        }
+        for(int j=1;j<=10;j++){
+            System.out.println(+b+"*" +j+"="+(b*j));
+        }
+    }
+  }
