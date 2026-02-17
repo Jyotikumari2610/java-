27d6ba379e9069808de175382c2f99e471e9ee16
Jyoti Kumari <jk1076387@gmail.com>
2026-02-17 23:21:27 +0530

    Update Practice3.java

--- Practice3.java
+++ Practice3.java
@@ -252,3 +252,36 @@ public class GuessGame{
         System.out.println("\n👋 Thanks for playing! See you next time!");
     }
 }
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+    /*  String prefix=strs[0];
+      for(int i=0;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        } 
+      return prefix;
+      */
+      String prefix="";
+      for(int i=0;i<strs[0].length();i++){
+        char ch=strs[0].charAt(i);
+        for(int j=1;j<strs.length;j++){
+            if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
+            return prefix;
+            }
+         }
+         prefix+=ch;
+     }
+    return prefix;
+  }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String strs[]=new String[n];
+        for(int i=0;i<n;i++){
+            strs[i]=sc.nextLine();
+        }
+        Solution obj=new Solution();
+        obj.longestCommonPrefix(strs);
+    }
+}
